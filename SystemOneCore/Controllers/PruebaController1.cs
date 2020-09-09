88e8de3799a240efefbaf4d3e81b5abb6639cb82
Bryan Ernesto Martínez Pleitez <bryan.martinezpleitez99@outlook.com>
2020-09-08 19:03:04 -0600

    Prueba de actualización y mantenimiento de repositorio.

---
+++ SystemOneCore/Controllers/PruebaController1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SystemOneCore.Controllers
+{
+    public class PruebaController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
